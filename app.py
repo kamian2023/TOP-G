abd547c1bfc4dba8cd84c06f57cba51ba196d5f8
kamian <aktermahfuza2022@gmail.com>
2023-03-16 19:55:48 +0600

    change GOOGLE ADSENSE

--- app.py
+++ app.py
@@ -70,3 +70,18 @@ components.html(
      crossorigin="anonymous"></script>
      """
 )
+components.html(
+"""
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2863019331434652"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-fb+5w+4e-db+86"
+     data-ad-client="ca-pub-2863019331434652"
+     data-ad-slot="6748562942"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+"""
+)
